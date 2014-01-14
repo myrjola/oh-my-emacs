8ffb7aa7d8df7802a08f0013235a9d1dcab1920d
Martin Yrjölä <martin.yrjola@gmail.com>
2014-01-14 22:17:41 +0200

    Fix evil-nerd-commenter operator

--- custom.el
+++ custom.el
@@ -1,13 +1,9 @@
-;;; This file contains some temporary code snippets, it will be loaded after
-;;; various oh-my-emacs modules. When you just want to test some code snippets
-;;; and don't want to bother with the huge ome.*org files, you can put things
-;;; here.
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evilnc-hotkey-comment-operator ",cn"))
+ '(evilnc-hotkey-comment-operator ",cc"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
