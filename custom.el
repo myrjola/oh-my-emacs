bd3efd5cbdefc82a24199ac010fcaa710940d0cd
Martin Yrjölä <martin.yrjola@gmail.com>
2014-01-18 16:42:11 +0200

    Fix curly braces in js3-mode.

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evilnc-hotkey-comment-operator ",cc"))
+ '(evilnc-hotkey-comment-operator ",cc")
+ '(magit-save-some-buffers (quote dontask)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
