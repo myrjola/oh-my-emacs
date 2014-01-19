7de83df555f7968afba4f965a3fb9819d023fcb2
myrjola <martin.yrjola@gmail.com>
2014-01-19 22:19:01 +0200

    merge confilcts

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(evilnc-hotkey-comment-operator ",cc")
- '(magit-save-some-buffers (quote dontask)))
+ '(magit-save-some-buffers (quote dontask))
+ '(org-attach-method (quote lns))
+ '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("\\.pdf\\'" . "okular %s")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
