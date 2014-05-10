7ef20cdb66e6882038af3cd0704fcf712852c99f
myrjola <martin.yrjola@gmail.com>
2014-05-11 00:59:11 +0300

    Many new changes on Sun May 11 00:59:20 2014

--- custom.el
+++ custom.el
@@ -5,9 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(evilnc-hotkey-comment-operator ",cc")
  '(magit-save-some-buffers (quote dontask))
+ '(magit-use-overlays nil)
  '(org-attach-method (quote lns))
  '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("\\.pdf\\'" . "okular %s"))))
- '(safe-local-variable-values (quote ((python-shell-interpreter-args . "/home/martin/git/djangotutorial/manage.py shell") (python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
+ '(rbenv-show-active-ruby-in-modeline nil)
+ '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python") (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python" :pythonpath "/home/martin/git/neverlate/django" :settings "neverlate.settings") (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python" :pythonpath "/home/david/megacorp/libs/projectzero" :settings "neverlate.settings") (python-shell-interpreter . "python2") (python-shell-interpreter-args . "/home/martin/git/djangotutorial/manage.py shell") (python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
 ") (python-shell-completion-module-string-code . "';'.join(module_completion('''%s'''))
 ") (python-shell-completion-setup-code . "from IPython.core.completerlib import module_completion") (python-shell-interpreter-args . "/home/martin/git/neverlate/django/manage.py shell") (python-shell-interpreter . "python")))))
 (custom-set-faces
