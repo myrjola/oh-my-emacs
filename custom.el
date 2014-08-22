68f2318596ce99b2d54c37770756e63659ad551d
myrjola <martin.yrjola@gmail.com>
2014-08-22 15:13:25 +0300

    Hide scroll-bars

--- custom.el
+++ custom.el
@@ -3,15 +3,48 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(evilnc-hotkey-comment-operator ",cc")
  '(magit-save-some-buffers (quote dontask))
  '(magit-use-overlays nil)
  '(org-attach-method (quote lns))
- '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("\\.pdf\\'" . "okular %s"))))
+ '(org-file-apps
+   (quote
+    ((auto-mode . emacs)
+     ("\\.mm\\'" . default)
+     ("\\.x?html?\\'" . default)
+     ("\\.pdf\\'" . "okular %s"))))
  '(rbenv-show-active-ruby-in-modeline nil)
- '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face tabs trailing lines-tail) (require-final-newline . t) (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python") (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python" :pythonpath "/home/martin/git/neverlate/django" :settings "neverlate.settings") (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python" :pythonpath "/home/david/megacorp/libs/projectzero" :settings "neverlate.settings") (python-shell-interpreter . "python2") (python-shell-interpreter-args . "/home/martin/git/djangotutorial/manage.py shell") (python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
-") (python-shell-completion-module-string-code . "';'.join(module_completion('''%s'''))
-") (python-shell-completion-setup-code . "from IPython.core.completerlib import module_completion") (python-shell-interpreter-args . "/home/martin/git/neverlate/django/manage.py shell") (python-shell-interpreter . "python")))))
+ '(safe-local-variable-values
+   (quote
+    ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace)
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (whitespace-line-column . 80)
+     (whitespace-style face tabs trailing lines-tail)
+     (require-final-newline . t)
+     (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python")
+     (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python" :pythonpath "/home/martin/git/neverlate/django" :settings "neverlate.settings")
+     (pony-settings make-pony-project :python "/home/martin/git/neverlate/neverlate_ve/bin/python" :pythonpath "/home/david/megacorp/libs/projectzero" :settings "neverlate.settings")
+     (python-shell-interpreter . "python2")
+     (python-shell-interpreter-args . "/home/martin/git/djangotutorial/manage.py shell")
+     (python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
+")
+     (python-shell-completion-module-string-code . "';'.join(module_completion('''%s'''))
+")
+     (python-shell-completion-setup-code . "from IPython.core.completerlib import module_completion")
+     (python-shell-interpreter-args . "/home/martin/git/neverlate/django/manage.py shell")
+     (python-shell-interpreter . "python")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
