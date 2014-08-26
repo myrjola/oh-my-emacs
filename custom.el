a8555c2efe28a9cd4f2275db2effa60160287622
myrjola <martin.yrjola@gmail.com>
2014-08-26 09:48:42 +0300

    Tue Aug 26 09:49:00 2014

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("b8714d3e17ae1b52e42ceb8ddeb41f49cd635cb38efc48ee05bf070c10a3268f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(evilnc-hotkey-comment-operator ",cc")
  '(magit-save-some-buffers (quote dontask))
  '(magit-use-overlays nil)
